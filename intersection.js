bb67b309dd1c06d087863d82b20da8071efb3268
Alan <alanleeyueh@gmail.com>
2022-10-03 21:00:57 -0400

    intersection

---
+++ intersection.js
@@ -0,0 +1,14 @@
+const intersection = (a, b) => {
+    const result = [];
+    const setA = new Set(a);
+    for (let item of b) {
+      if (setA.has(item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  };
+  module.exports = {
+    intersection,
+  };
+  
