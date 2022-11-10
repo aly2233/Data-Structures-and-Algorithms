19943e9542025894a4e71bc212021565f3b1830a
Alan <alanleeyueh@gmail.com>
2022-11-10 12:18:19 -0500

    sumlist

---
+++ sumlist.js
@@ -0,0 +1,10 @@
+const sumList = (head) => {
+    let sum = 0;
+    let current = head;
+    
+    while (current !== null) {
+      sum += current.val;
+      current = current.next;
+    }
+    return sum;
+  };
