69fdc224455aa2bc58e02a3b16eb09f98fa9fb8c
Alan <alanleeyueh@gmail.com>
2023-04-03 23:57:06 -0400

    isUnivalueList

---
+++ isUnivalueList.js
@@ -0,0 +1,10 @@
+const isUnivalueList = (head) => {
+    let current = head;
+    
+    while (current !== null) {
+      if (current.val !== head.val) return false;
+      current = current.next;
+    }
+    
+    return true;
+  }
